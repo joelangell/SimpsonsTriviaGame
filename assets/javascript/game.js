c31af980a6f1b3721e5cee6d4c1d1d7a82393de0
Joel Angell <j.angell07@gmail.com>
2018-02-24 00:52:42 -0800

    Small update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -166,6 +166,7 @@ $(document).ready(function () {
 
 
     $(".list-group-item-success").on("click", function () {
+        $(this).off('click')
         $("#answerBox").show()
         stopTimer = true
         if (($(this).text()) === questions[answered][5]) {
@@ -177,6 +178,7 @@ $(document).ready(function () {
             var newQuestion = setTimeout(function () {
                 answered++
                 emptyQuestion()
+                $(this).off('click')
             }, 7000)
         }
         else if (($(this).text()) !== questions[answered][5]) {
@@ -188,6 +190,7 @@ $(document).ready(function () {
             var newQuestion = setTimeout(function () {
                 answered++
                 emptyQuestion()
+                $(this).off('click')
             }, 7000)
         }
 
